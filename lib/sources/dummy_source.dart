f6a1a9245354e42c95a2f5cf02c0c5dc2ad717ad
Seweryn Woliński <42438445+saviusz@users.noreply.github.com>
2025-11-08 22:49:13 +0100

    feat: create sample songs in DummySource

--- lib/sources/dummy_source.dart
+++ lib/sources/dummy_source.dart
@@ -1,3 +1,11 @@
+import 'package:melon_app/models/song.dart';
 import 'package:melon_app/sources/abstract_source.dart';
+
+final songs = [
+  Song("-dummy-1", titles: ["Piosenka"]),
+  Song("-dummy-2", titles: ["Główny tytuł", "Tytuł dodatkowy"]),
+  Song("-dummy-3", titles: ["To będzie naprawde długi tytuł"]),
+  Song("-dummy-4", titles: ["Piosenka pisana nocą"]),
+];
 
 class DummySource extends AbstractSource {}
